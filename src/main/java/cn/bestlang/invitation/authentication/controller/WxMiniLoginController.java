355e424142bfe97230a778593b47ac7cb28a2209
gen <ligen60@jd.com>
2021-03-27 19:13:49 +0800

    fix wx-mp login

--- src/main/java/cn/bestlang/invitation/authentication/controller/WxMiniLoginController.java
+++ src/main/java/cn/bestlang/invitation/authentication/controller/WxMiniLoginController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Slf4j
@@ -20,12 +21,22 @@ public class WxMiniLoginController {
     @Autowired
     private WxMiniLoginService wxMiniLoginService;
 
+    /**
+     * spring mvc ServletRequestMethodArgumentResolver 解析 HttpSession httpSession时,
+     * 使用 HttpSession session = request.getSession(); 会出发session 创建。
+     *
+     * 所以此处为了避免函数开始时创建 session,  使用 HttpServletRequest request 作为 参数
+     * 最后 成功后才 通过 request.getSession() 创建 session。
+     *
+     * @param request
+     * @param wxMiniLoginReq
+     */
     @PostMapping("/login")
-    public void login(HttpSession httpSession, @Validated @RequestBody WxMiniLoginReq wxMiniLoginReq) {
+    public void login(HttpServletRequest request , @Validated @RequestBody WxMiniLoginReq wxMiniLoginReq) {
         log.info("login wxMiniLoginReq : {}", JsonUtil.toJson(wxMiniLoginReq));
 
         User user = wxMiniLoginService.login(wxMiniLoginReq);
-        SimpleSecurityContext.saveAuthentication(httpSession, user.getId());
+        SimpleSecurityContext.saveAuthentication(request.getSession(), user.getId());
     }
 
     @GetMapping("/mlogin/{userId}")
