5a52da16ff135f1c0fcd8fea55da0a5c549d9d94
gen <ligen60@jd.com>
2021-03-28 00:35:42 +0800

    query comments sort by time desc

--- src/main/java/cn/bestlang/invitation/repo/CommentRepository.java
+++ src/main/java/cn/bestlang/invitation/repo/CommentRepository.java
@@ -7,5 +7,18 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CommentRepository extends MongoRepository<Comment, String> {
 
-    Page<Comment> findAllByTopic(String topic, Pageable var1);
+    /**
+     *
+     * spring data 源码 org.springframework.data.repository.query.parser.PartTree.java
+     * 解析方法名， 内部类 Predicate 分割排序时 使用 "OrderBy" ，而不是文档中的 "SortBy"
+     *
+     * https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/#mongodb.repositories.queries.sort
+     *
+     *
+     *
+     * @param topic
+     * @param var1
+     * @return
+     */
+    Page<Comment> findAllByTopicOrderByTimeDesc(String topic, Pageable var1);
 }
